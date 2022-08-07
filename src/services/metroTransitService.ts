a3367ed16b9886627c39e7d6ce076588475910fe
Joshua Gossett <jgossett@brighton-science.com>
2022-08-07 18:04:36 -0500

    feat(application): resolve runtime bug.

--- src/services/metroTransitService.ts
+++ src/services/metroTransitService.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from '@/services/constants';
-import { NexTripApi } from '@/services/generated-nextrip-api/index.ts'
+import { NexTripApi } from '@/services/generated-nextrip-api/index'
 import axios from 'axios';
 
 const axiosInstance = axios.create();
